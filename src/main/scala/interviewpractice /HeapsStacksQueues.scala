c9c89332f27ec3cd63d10fe05c6f8efa08874523
V. Q. Tran <vietsemantics@gmail.com>
2020-03-16 00:34:15 -0500

    HeapsStacksQueues

--- src/main/scala/interviewpractice /HeapsStacksQueues.scala
+++ src/main/scala/interviewpractice /HeapsStacksQueues.scala
@@ -28,7 +28,32 @@ object HeapsStacksQueues {
 
 //  https://app.codesignal.com/interview-practice/task/aRwxhGcmvhf6vKPCp
   def simplifyPath(path: String): String = {
-    ""
+    var p = path
+    var clean = false
+    do {
+      val x = p.replaceAll("([/]{2})|(/\\./)", "/")
+      clean = x.length == p.length
+      p = x
+    }
+    while (!clean)
+    val a = p.split("/")
+    var r = Array[String]()
+    for (s <- a) {
+      if (s.equals("..")) {
+        if (r.length > 0) {
+          r = r.tail
+        }
+      } else {
+        r = s +: r
+      }
+    }
+    val result = r.reverse.mkString("/")
+    if (!result.startsWith("/")) {
+      "/" + result
+    } else {
+      result
+    }
+
   }
 
 
